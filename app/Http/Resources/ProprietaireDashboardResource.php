b03e9c4b0d9b9cbfc90ab413992b75b571fa75f0
Ibrahima Ndir <ibrahimandir2410@gmail.com>
2026-09-16 11:31:30 +0000

    fix: add float cast to taux_recouvrement in ProprietaireDashboardResource.php

--- app/Http/Resources/ProprietaireDashboardResource.php
+++ app/Http/Resources/ProprietaireDashboardResource.php
@@ -25,13 +25,9 @@ class ProprietaireDashboardResource extends JsonResource
                 'paiements_attendus' => (int)($this['stats_mois_en_cours']['paiements_attendus'] ?? 0),
                 'revenus_potentiels' => (int)($this['stats_mois_en_cours']['revenus_potentiels'] ?? 0),
                 'paiements_en_retard' => (int)($this['stats_mois_en_cours']['paiements_en_retard'] ?? 0),
-<<<<<<< HEAD
-                'taux_recouvrement' => $this['stats_mois_en_cours']['taux_recouvrement'] ?? 0,
+                'taux_recouvrement' => (float)($this['stats_mois_en_cours']['taux_recouvrement'] ?? 0.0),
                 'loyers_manuels_du_mois' => (int)($this['stats_mois_en_cours']['loyers_manuels_du_mois'] ?? 0),
                 'afficher_alerte_retention' => (bool)($this['stats_mois_en_cours']['afficher_alerte_retention'] ?? false),
-=======
-                'taux_recouvrement' => (float)($this['stats_mois_en_cours']['taux_recouvrement'] ?? 0),
->>>>>>> 9fbdc60 (Mise à jour économique, payouts, demandes, et ressources)
             ],
             'subscription' => isset($this['subscription'])
                 ? [
